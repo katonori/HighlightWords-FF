40bc3a565fbf01c5f683d8e52a12b4e9b5c12605
katonori <katonori.d@gmail.com>
2014-11-30 23:53:31 +0900

    Remove unused parts.

--- bootstrap.js
+++ bootstrap.js
@@ -211,12 +211,8 @@ gSearchWP.Highlighter.NodeHighlighter = function(aName) {
           element = elementProto.cloneNode(false);
           element.appendChild( towrap );
           fragment.appendChild( element );
-
-          debug("ELEMENT:0: " + element.range + "," + j + "," + n);
-
         // others
         } else {
-          debug("ELEMENT:1: " + element.range);
           element = elementProto.cloneNode(false);
           node.parentNode.replaceChild( element, node );
           element.appendChild( node );
@@ -251,13 +247,6 @@ gSearchWP.Highlighting = new function() {
 
     var tabBox = document.getElementById("content").mTabBox;
     tabBox.addEventListener("select", refreshCallback, false);
-
-    /*
-    if (this.highlightButton) {
-      this.highlightButton.setAttribute("checked", gSearchWP.Preferences.highlighted);
-      this.highlightButton.setAttribute("matchcase", gSearchWP.Preferences.highlightMatchCase);
-    }
-    */
   }
 
   /**
@@ -351,7 +340,6 @@ gSearchWP.Highlighting = new function() {
    */
   //function highlight() {
   this.highlight = function () {
-    debug("highlight start");
     var termsArray = getChromeWindow()._extensionHilighterWords;
     for(var i = 0, len = termsArray.length; i < len; ++i) {
         debug("WORDS: " + termsArray[i]);
@@ -365,20 +353,7 @@ gSearchWP.Highlighting = new function() {
         var criteria = "term-" + ( i % highlighterCount + 1 );
         count += highlightBrowserWindow( termsArray[i], criteria, highlightMatchCase );
       }
-
-      /*
-      if (count > 1) {
-        gSearchWP.displayMessage(_stringBundle.getFormattedString("highlightCountN", [count], 1), false);
-      }
-      else if (count == 1) {
-        gSearchWP.displayMessage(_stringBundle.getFormattedString("highlightCount1", [count], 1), false);
-      }
-      else {
-        gSearchWP.displayMessage(_stringBundle.getString("highlightCount0"), false);
-      }
-      */
     }
-    debug("highlight done");
   }
 
   /**
@@ -410,13 +385,6 @@ gSearchWP.Highlighting = new function() {
 
     if ( !clearing && overlapsDisplayMode == 1 ) { // fixed
       doc.body.classList.add("searchwp-overlaps-display-mode-1");
-    } else {
-      doc.body.classList.remove("searchwp-overlaps-display-mode-1");
-    }
-
-    if ( !clearing && overlapsDisplayMode == 2 ) { // transparent
-      doc.body.classList.add("searchwp-overlaps-display-mode-2");
-    } else {
       doc.body.classList.remove("searchwp-overlaps-display-mode-2");
     }
 
@@ -449,115 +417,20 @@ gSearchWP.Highlighting = new function() {
 
         count = searchResults.length;
       }
-
-      if ( overlapsDisplayMode == 3 ) { // multiply
-        Array.forEach(doc.body.querySelectorAll(".searchwp-term > .searchwp-term"), recalculateColors);
-      }
     }
 
     return count;
   }
 
-  function recalculateColors( node ) {
-    if ( !node._searchwp_recalculated_rgb ) {
-      var parent = node.parentNode;
-      var upperRgb = term2RGB[ node.getAttribute("highlight") ];
-      var lowerRgb = parent._searchwp_recalculated_rgb || term2RGB[ parent.getAttribute("highlight") ];
-      var rgb = combineColors( upperRgb, lowerRgb, chanelBlanding.mutiply );
-      var color = rgbLuminance( rgb ) > 165 ? "black" : "white";
-
-      node._searchwp_recalculated_rgb = rgb;
-
-      node.style.backgroundColor = "rgb(" + rgb + ")";
-      node.style.color = color;
-    }
-  }
-
-  var term2RGB = {
-    "term-1": [ 251, 237, 115 ],
-    "term-2": [ 255, 177, 140 ],
-    "term-3": [ 255, 210, 129 ],
-    "term-4": [ 195, 249, 145 ],
-    "term-5": [ 233, 184, 255 ]
-  };
-
-  var colorTbl = [
-      "rgb(251, 237, 115)",
-      "rgb(255, 177, 140)",
-      "rgb(255, 210, 129)",
-      "rgb(195, 249, 145)",
-      "rgb(233, 184, 255)",
-  ];
-
-  var chanelBlanding = {
-    mutiply: function( a, b ) {
-      return Math.round( a / 255 * b );
-    },
-    difference: function( a, b ) {
-      return Math.abs( a - b );
-    }
-  };
-
-  function combineColors( a, b, method ) {
-    return [
-      method( a[0], b[0] ),
-      method( a[1], b[1] ),
-      method( a[2], b[2] )
-    ];
-  }
-
-  function rgbLuminance( rgb ) {
-    return Math.min(255, Math.round( 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2] ));
-  }
-
-  var colorIdx = 0;
   function createElementProto( aDocument, aCriteria ) {
     var element = aDocument.createElement("layer");
     element.className = "searchwp-term";
     element.setAttribute( "highlight", aCriteria );
-    /*
-    element.style.backgroundColor = colorTbl[colorIdx];
-    debug("COLOR: " + colorTbl[colorIdx]);
-    ++colorIdx;
-    if(colorIdx >= colorTbl.length) {
-        colorIdx = 0;
-    }
-    */
     return element;
   }
 
-  /**
-   * SoundexMatcher for the NodeSearcher.
-   */
-  function SoundexMatcher(aCriteria) {
-    var _soundex = soundex(aCriteria);
-
-    this.match = function(str) {
-      var matches = str.match(/\b\w+\b/gi);
-      if (matches) {
-        for (var i = 0; i < matches.length; i++) {
-          if (soundex(matches[i]) == _soundex) {
-            return matches[i];
-          }
-        }
-      }
-      return null;
-    }
-
-    function soundex(str, p) {
-      p = isNaN(p) ? 4 : p > 10 ? 10 : p < 4 ? 4 : p;
-      var i, j, r, m = {BFPV: 1, CGJKQSXZ: 2, DT: 3, L: 4, MN: 5, R: 6},
-        r = (s = str.toUpperCase().replace(/[^A-Z]/g, "").split("")).splice(0, 1);
-      for (i in s) {
-        for(j in m) {
-          if(j.indexOf(s[i]) + 1 && r[r.length-1] != m[j] && r.push(m[j])) break;
-        }
-      }
-      return r.length > p && (r.length = p), r.join("") + (new Array(p - r.length + 1)).join("0");
-    }
-  }
-
   function getFindSelection( aWindow ) {
+    debug("DDDDDDDDD: getFindSelection()");
     return gSearchWP.getSelectionOfType( aWindow, 128 );
   }
 
@@ -624,16 +497,7 @@ let OneHandZoom = {
         let deck = aWindow.BrowserApp.deck;
         deck.addEventListener("touchstart", this, true);
         deck.addEventListener("pageshow", this, false);
-        deck.addEventListener("DOMContentLoaded", this, false);
-        deck.addEventListener("TabOpen", this, false);
-        let flags = Ci.nsIWebProgress.NOTIFY_STATE_ALL |
-            Ci.nsIWebProgress.NOTIFY_LOCATION |
-            Ci.nsIWebProgress.NOTIFY_SECURITY;
-        //getChromeWindow().addProgressListener(this, flags);
-        Services.obs.addObserver(this, "Tab:Load", false);
         Services.obs.addObserver(this, "Tab:Selected", false);
-        Services.obs.addObserver(this, "Content:StateChange", false);
-        Services.obs.addObserver(this, "Content:ReaderEnabled", false);
     },
 
     unload: function(aWindow) {
@@ -645,23 +509,13 @@ let OneHandZoom = {
         let deck = aWindow.BrowserApp.deck;
         deck.removeEventListener('touchstart', this, true);
         deck.removeEventListener("pageshow", this, false);
-        deck.removeEventListener("DOMContentLoaded", this, false);
-        deck.removeEventListener("TabOpen", this, false);
-        let flags = Ci.nsIWebProgress.NOTIFY_STATE_ALL |
-            Ci.nsIWebProgress.NOTIFY_LOCATION |
-            Ci.nsIWebProgress.NOTIFY_SECURITY;
-        //aWindow.BrowserApp.removeProgressListener(this, flags);
-        Services.obs.removeObserver(this, "Tab:Load", false);
         Services.obs.removeObserver(this, "Tab:Selected", false);
-        Services.obs.removeObserver(this, "Content:StateChange", false);
-        Services.obs.removeObserver(this, "Content:ReaderEnabled", false);
     },
 
     observe: function(aSubject, aTopic, aData) {
         debug('observe(' + aSubject + ', ' + aTopic + ', ' + aData + ')');
 
         switch (aTopic) {
-            case 'Tab:Load':
             case 'Tab:Selected':
                 debug("onloadCb");
                 var uri = getTabUrl(aData);
@@ -674,25 +528,18 @@ let OneHandZoom = {
         debug("uri: " + uri);
         var match = gSyncRegex.exec(uri);
         if(match) {
-            for(var i = 0, len = match.length; i < len; ++i) {
-                debug("DDDDDDDDDDDDD match: " + match[i]);
-            }
             var matchStr = match[match.length-1];
             var words = [];
             if(matchStr.indexOf("+") >= 0) {
                 words = match[match.length-1].split("+");
-                for(var i = 0, len = words.length; i < len; ++i) {
-                    debug("DDDDD: " + words[i]);
-                }
             }
             else {
                 words = [matchStr];
             }
             for(var i = 0, len = words.length; i < len; ++i) {
                 words[i] = decodeURI(words[i]);
-                debug("DDDDD: " + words[i]);
+                debug("Words: " + words[i]);
             }
-            debug("DDDDD: words: " + words.length);
             getChromeWindow()._extensionHilighterWords = words;
         }
     },
@@ -700,33 +547,14 @@ let OneHandZoom = {
     handleEvent: function(aEvent) {
         debug("handleEvent: " + aEvent.type);
         switch (aEvent.type) {
-            /*
-            case 'TabOpen':
-            {
-                let flags = Ci.nsIWebProgress.NOTIFY_STATE_ALL;
-                getSelectedTab().browser.addProgressListener(progressListener, flags);
-                break;
-            }
-            */
-            /*
-            case 'DOMContentLoaded':
-            {
-                debug("DDDDDDDDDDDDDDD: DOMContentLoaded");
-                var uri = getCurTabUrl();
-                this._updateHighlightWords(uri);
-                highlightStart();
-                break;
-            }
-            */
             case 'pageshow':
             {
                 if (aEvent.originalTarget.defaultView != getSelectedTab().browser.contentWindow) {
                     break;
                 }
-                debug("DDDDDDDDDDDDDDD:1: pageshow");
                 var uri = getCurTabUrl();
                 this._updateHighlightWords(uri);
-                highlightStart();
+                gSearchWP.Highlighting.highlight();
                 break;
             }
         }
@@ -737,39 +565,6 @@ let OneHandZoom = {
         }
     },
 };
-
-/*
-var progressListener =
-{
-    QueryInterface: function(aIID)   
-    {
-        return this;
-    },
-     
-    onStateChange: function(aWebProgress, aRequest, aFlag, aStatus)
-    {
-        if( ( aFlag & Ci.nsIWebProgressListener.STATE_IS_WINDOW )
-                && ( aFlag & Ci.nsIWebProgressListener.STATE_STOP ) ) {
-            debug("LOAD END: " + aRequest + "," + aFlag + "," + aStatus + "," + aWebProgress);
-            if(aRequest) {
-                debug("REQ: " + aRequest.loadFlags + "," +aRequest.name + "," + aRequest.loadGroup.activeCount);
-            }
-            debug(getCurTabUrl());
-        }
-        return 0;
-    },   
-   
-    onLocationChange: function(aProgress, aRequest, aURI)
-    {
-        debug("onLocationChange");
-        return 0;
-    },
-
-    onProgressChange: function(aWebProgress, aRequest, curSelf, maxSelf, curTot, maxTot) { return 0;},
-    onStatusChange: function(aWebProgress, aRequest, aStatus, aMessage) { return 0;},
-    onSecurityChange: function(aWebProgress, aRequest, aState) { return 0;}   
-};
-*/
 
 //===========================================
 // bootstrap.js API
@@ -907,6 +702,3 @@ function tr(aName) {
     }
 }
 
-function highlightStart() {
-    gSearchWP.Highlighting.highlight();
-}
