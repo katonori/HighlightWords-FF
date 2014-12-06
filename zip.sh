cc046b566403253efcf9c7e89b3f991b86bca7c1
katonori <katonori.d@gmail.com>
2014-12-06 19:30:16 +0900

    Remove unused files.

--- zip.sh
+++ zip.sh
@@ -1,2 +1,2 @@
-zip -r a.xpi Changelog.txt LICENSE.txt README.mkd bootstrap.js chrome.manifest content/ icon.png icon64.png install.rdf locale/
+zip -r a.xpi Changelog.txt LICENSE.txt README.mkd bootstrap.js chrome.manifest content/ icon.png icon64.png install.rdf 
 cp a.xpi /var/www/html/
