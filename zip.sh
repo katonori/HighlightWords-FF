0c8d05a9c9d52821dfdb2a2bf20e4f0b8c1e8a97
katonori <katonori.d@gmail.com>
2014-12-08 03:40:16 +0900

    Update Changelog.txt and zip.sh.

--- zip.sh
+++ zip.sh
@@ -1,4 +1,5 @@
 rm -f a.xpi
+grep -rE "DEBUG|TEST" . | grep const
 zip -r a.xpi Changelog.txt LICENSE.txt README.md bootstrap.js chrome.manifest content/highlighting-user.css content/nodeHighlighter.js content/nodeSearcher.js content/highlightWords.js icon.png icon64.png install.rdf 
 cp a.xpi /var/www/html/
 cp a.xpi HighlightWords-1.0.0.xpi
