7db6e612dc68b7995295ca756fa9ff8fbd7b0c31
katonori <katonori.d@gmail.com>
2014-12-07 14:40:27 +0900

    Update a packager for test mode.

--- zip_test.sh
+++ zip_test.sh
@@ -1,2 +1,2 @@
-zip -r a.xpi Changelog.txt LICENSE.txt README.mkd bootstrap.js chrome.manifest content/ test/ icon.png icon64.png install.rdf 
+zip -r a.xpi Changelog.txt LICENSE.txt README.md bootstrap.js chrome.manifest content/testModule.js content/highlighting-user.css content/nodeHighlighter.js content/nodeSearcher.js icon.png icon64.png install.rdf 
 cp a.xpi /var/www/html/
